13b8c295289fa4b175f40f6929bc62e1217c8a60
Mashael <m.alsaeedi@safcsp.org.sa>
2021-04-25 04:31:40 +0300

    binary search array

---
+++ BinarySearchArray.js
@@ -0,0 +1,39 @@
+
+class BinarySearchArray {
+
+
+
+/**
+ * @constructor 
+ * @param {Array} list
+ */
+
+    constructor(list=[]){
+        this.list = list
+    }
+
+/**
+ * @search Search 
+ * @param {number} target the value of the node we are searching fo
+ * Algorithm Inorder(tree)
+ * 1. check if the target value is larger, lower or equal to the root value 
+ * 2. if target is lower, go left. if it's larger, go right.
+ * 3. recursion until it finds the value or else returns null 
+ */
+
+    search(target, low=0, high=this.list.length-1, data=this.list){
+        let mid =  Math.floor((high + low) / 2)
+        if(target === data[mid]) return mid
+        if(target > data[mid]) return this.search(target,mid, data.length+1)
+        if(target < data[mid]) return this.search(target, 0, mid)
+        return null
+    }
+}
+
+
+let tree = new BinarySearchArray([0,1,3,5,7,8,18,20,24]);
+console.log('Searching for 20 ', tree.search(20))
+console.log('Searching for 1 ', tree.search(1))
+console.log('Searching for 0 ', tree.search(0))
+console.log('Searching for 100 ', tree.search(100))
+
