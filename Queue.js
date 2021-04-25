b1c3d54a0437a5e4fdd93ea780f050737b618745
Mashael <m.alsaeedi@safcsp.org.sa>
2021-04-25 04:31:51 +0300

    WIP queue

---
+++ Queue.js
@@ -0,0 +1,31 @@
+class Queue {
+    constructor(){
+        this.queue = [];
+        this.tail = 0;
+        this.head = 0;
+    }
+
+    enqueue(element) {
+        this.queue[this.tail++] = element;
+    }
+
+    size() {
+        return this.queue.filter((element) => element !== null).length
+    }
+
+    peek(){
+      return this.queue[this.head];
+    }
+
+    dequeue() {
+    if (this.tail === this.head)
+        return undefined
+
+    let element = this.queue[this.head];
+    delete this.queue[this.head];
+    this.head +=1;
+    return element;
+    }
+}
+
+module.exports = Queue
