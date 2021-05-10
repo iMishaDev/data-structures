27a82d06a4287311d1ba0a6e1f69c10c7835c2d2
Mashael <m.alsaeedi@safcsp.org.sa>
2021-05-10 06:23:31 +0300

    update stack

--- Stack.js
+++ Stack.js
@@ -4,16 +4,20 @@ class Stack {
     constructor(){
         this.stack = []
         this.head = -1;
+        this.maxes = []
     }
 
 
     push(element){
         ++this.head;
         this.stack[this.head] = element
+        this.maxes.push(element)
     }
 
     pop(){
         let element = this.stack[this.head]
+        this.maxes.splice(this.head,1)
+
         this.stack.length -= 1;
         --this.head;
         return element
@@ -37,26 +41,23 @@ class Stack {
         }
         return -1;
     }
-
-    print(){
-        this.stack.map((element) => console.log(element))
-    }
 }
 
-
-
 let stack = new Stack([]);
-console.log('push 10', stack.push(10))
-console.log('push 9 ', stack.push(9))
-console.log('pop ', stack.pop())
-console.log('push 1 ', stack.push(1))
-console.log('length', stack.length())
-console.log('isEmpty', stack.isEmpty())
-console.log('peek', stack.peek())
-console.log('searching for 10', stack.search(10))
-console.log('pop ', stack.pop())
-console.log('pop ', stack.pop())
-console.log('isEmpty', stack.isEmpty())
-console.log('peek', stack.peek())
+// console.log('push 9 ', stack.push(9))
 
-console.log(stack)
+// console.log('push 10', stack.push(10))
+// console.log('pop ', stack.pop())
+// console.log('push 1 ', stack.push(1))
+// console.log('length', stack.length())
+// console.log('isEmpty', stack.isEmpty())
+// console.log('peek', stack.peek())
+// console.log('searching for 10', stack.search(10))
+// console.log('pop ', stack.pop())
+// console.log('pop ', stack.pop())
+// console.log('isEmpty', stack.isEmpty())
+// console.log('peek', stack.peek())
+// console.log('max', stack.max())
+// console.log(stack)
+
+console.log(Stack.productExceptSelf([1, 2, 3, 4]))
