1b9426d6bda5f4c7e965ea55fbf8101070068807
Mashael <m.alsaeedi@safcsp.org.sa>
2021-05-12 07:24:47 +0300

    add heapifyDown into Heap

--- Heap.js
+++ Heap.js
@@ -20,11 +20,11 @@ class Heap {
         return Math.floor((index - 1) / 2)
     }
 
-    getLeftChildIndex(){
+    getLeftChildIndex(index){
         return (index * 2) + 1
     }
 
-    getRightChildIndex(){
+    getRightChildIndex(index){
         return (index * 2) + 2
     }
 
@@ -53,7 +53,6 @@ class Heap {
     add(element){
         this.heap[this.heap.length] = element
         this.#heapifyUp()
-        return this.heap[this.heap.length - 1]
     }
 
     poll(){
@@ -75,7 +74,21 @@ class Heap {
     }
 
     #heapifyDown(){
+        let index = 0, smallest = 0
+        
 
+        while(this.hasLeftChild(index)){
+            smallest = this.getLeftChildIndex(index);
+            if(this.hasRightChild(index) && this.getRightChild(index) < smallest){
+                smallest = this.getRightChildIndex
+            }
+            if(this.heap[index] < this.heap[smallest]) return
+            else {
+                [this.heap[smallest], this.heap[index]] =
+                this.#swap(this.heap[smallest], this.heap[index])
+                index = smallest
+            }
+        }
     }
 }
 
@@ -83,7 +96,7 @@ let heap = new Heap([]);
 console.log('add 100 ', heap.add(100))
 console.log('add 10 ', heap.add(10))
 console.log('add 9', heap.add(9))
-// console.log('poll ', heap.poll())
+console.log('poll ', heap.poll())
 console.log('add 1 ', heap.add(1))
 console.log('peek', heap.peek())
 console.log(heap)
