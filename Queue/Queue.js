074b1749cafe801acc1541d0625e4847bc3d7aa1
Mashael <m.alsaeedi@safcsp.org.sa>
2021-10-08 20:21:30 +0300

    fix reuglar queue dequeue

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -11,7 +11,7 @@ class Queue {
     }
 
     enqueue(element) {
-        if(this.head === this.queue.length -1)
+        if(this.tail === this.queue.length -1)
             return 'queue is full!';
 
         if(this.tail == -1){
@@ -33,9 +33,18 @@ class Queue {
     }
 
     dequeue() {
+        if(this.tail === this.head === -1)
+            return 'Queue is Empty';
+        
         let element = this.queue[this.head];
         delete this.queue[this.head];
-        this.head += 1;
+        
+        if( this.head + 1 === this.queue.length){
+            this.head = -1;
+            this.tail = -1;
+        } else {
+            this.head += 1;
+        }
 
         return element;
     }
@@ -53,6 +62,15 @@ console.log(queue.size())
 console.log(queue.queue)
 queue.dequeue();
 queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+console.log(queue.head, queue.tail)
 console.log(queue.queue)
 console.log(queue.size())
+queue.enqueue(5)
+queue.enqueue(6)
+console.log(queue.head, queue.tail)
+console.log(queue.queue)
 // module.exports = Queue
