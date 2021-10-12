b8b70563c313da9ea478413f1893612a3ec49bf5
Mashael <m.alsaeedi@safcsp.org.sa>
2021-10-12 19:25:56 +0300

    implement insert into red black tree

--- BinaryTree/RedBlackTree.js
+++ BinaryTree/RedBlackTree.js
@@ -1,5 +1,5 @@
 class RedBlackNode {
-    constructor(color='red', key, leftChild=null, rightChild=null, parent=null){
+    constructor(color=1, key, leftChild=null, rightChild=null, parent=null){
         this.color = color;
         this.key = key;
         this.leftChild = leftChild;
@@ -10,39 +10,160 @@ class RedBlackNode {
 
 
 class RedBlackTree {
-    constructor(root = null){
-        this.root = root;
+    constructor(){
+        this.root = null
     }
 
     insert(node){
         if(!this.root){
-            this.root = this.insertHelper(node, this.root);
+            this.root = new RedBlackNode(1, node, null, null, null);
+            return;
         }
-        else return this.insertHelper(node, this.root);
+        return this.insertHelper(node, this.root);
     }
 
     insertHelper(node, root){
-        if(!root){
-            root =  new RedBlackNode('red', node, null, null, null);
-        }
-        if(node > root.key){
-            
+        if(node > root.key){  
             if(root.rightChild)
-                this.insert(node, root.rightChild);
-            else root.rightChild = new RedBlackNode('red', node, null, null, root);
+                this.insertHelper(node, root.rightChild);
+            else {
+                root.rightChild = new RedBlackNode(1, node, null, null, root);
+                if (root.rightChild === null){
+                    root.rightChild.color = 0
+                    return
+                }
+
+                if (root.rightChild.parent === null || root.rightChild.parent.parent === null)
+                    return
+                this.fixInsert(root.rightChild);
+            }
         }
 
         if(node < root.key){
             if(root.leftChild)
-                this.insert(node, root.leftChild);
-            else root.leftChild = new RedBlackNode('red', node, null, null, root);
+                this.insertHelper(node, root.leftChild);
+            else {
+                root.leftChild = new RedBlackNode(1, node, null, null, root);
+                if (root.leftChild.parent === null){
+                    root.leftChild.color = 0
+                    return
+                }
+
+                if (root.leftChild.parent === null || root.leftChild.parent.parent === null)
+                    return
+                this.fixInsert(root.leftChild);
+            }
         }
         return root;
     }
+
+    fixInsert(element){
+        while (element.parent && element.parent.color == 1){
+            if ( element.parent.parent && element.parent == element.parent.parent.rightChild){
+                let u = element.parent.parent.leftChild
+                if (u.color == 1){
+                    u.color = 0
+                    element.parent.color = 0
+                    element.parent.parent.color = 1
+                    element = element.parent.parent
+                }
+                else{
+                    if (element == element.parent.leftChild){
+                        element = element.parent
+                        this.rightRotate(element)
+                    }
+                    element.parent.color = 0
+                    element.parent.parent.color = 1
+                    this.leftRotate(element.parent.parent)
+                }
+            }
+            else{
+                let u = element.parent.parent.rightChild
+
+                if (u.color == 1) {
+                    u.color = 0
+                    element.parent.color = 0
+                    element.parent.parent.color = 1
+                    element = element.parent.parent
+                }
+                else {
+                    if (element == element.parent.rightChild){
+                        element = element.parent
+                        this.leftRotate(element)
+                    }
+                    element.parent.color = 0
+                    element.parent.parent.color = 1
+                    this.rightRotate(element.parent.parent)
+                }
+            if (element == this.root)
+                break
+            }
+        }
+        this.root.color = 0
+    }
+
+
+
+
+    leftRotate( x){
+        let y = x.rightChild
+        x.rightChild = y.leftChild
+        if (y.leftChild && (y.leftChild.leftChild !== null || y.leftChild.rightChild !== null))
+            y.leftChild.parent = x
+
+        y.parent = x.parent
+        if (x.parent == null)
+            this.root = y
+        else if (x == x.parent.leftChild)
+            x.parent.leftChild = y
+        else {
+            x.parent.rightChild = y
+        }
+        y.leftChild = x
+        x.parent = y
+    }
+
+
+    rightRotate(x){
+        let y = x.leftChild
+        x.leftChild = y.rightChild
+        
+        if (y.rightChild && (y.rightChild.leftChild != null && y.rightChild.rightChild !== null))
+            y.rightChild.parent = x
+
+        y.parent = x.parent
+        if (x.parent == null)
+            this.root = y
+        else if( x == x.parent.rightChild)
+            x.parent.rightChild = y
+        else {
+            x.parent.leftChild = y
+        }
+        y.rightChild = x
+        x.parent = y
+    }
+
+    inorder(root=this.root){
+        if(!root)
+            return;
+        this.inorder(root.leftChild)
+        console.log(root.key, root.color)
+        this.inorder(root.rightChild)
+    } 
 }
 
 let tree = new RedBlackTree();
 
-tree.insert(4);
-tree.insert(10);
-console.log(tree)
+
+tree.insert(55)
+tree.insert(40)
+tree.insert(65)
+tree.insert(60)
+tree.insert(75)
+tree.insert(57)
+tree.inorder()
+console.log(tree.root.rightChild)
+
+/**
+ * ref: https://www.programiz.com/dsa/red-black-tree
+ */
