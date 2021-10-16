33f2de8f0614d948bdd7fbada37819411d036d16
Mashael <m.alsaeedi@safcsp.org.sa>
2021-10-16 15:12:11 +0300

    implement iterative binary search

--- BinaryTree/BinarySearchArray.js
+++ BinaryTree/BinarySearchArray.js
@@ -21,22 +21,48 @@ class BinarySearchArray {
  * 3. recursion until it finds the value or else returns null 
  */
 
-    search(target, low=0, high=this.list.length-1, data=this.list){
+    search_recursion(target, low=0, high=this.list.length-1, data=this.list){
         if(high < low)
             return -1;
         
         let mid =  Math.floor((high + low) / 2)
 
         if(target === data[mid]) return mid;
-        else if(target < data[mid]) return this.search(target, 0, mid)
-        else return this.search(target, mid + 1, data.length - 1)
+        else if(target < data[mid]) return this.search_recursion(target, 0, mid)
+        else return this.search_recursion(target, mid + 1, data.length - 1)
+    }
+
+
+    search_iterative(target, data=this.list){
+        let low = 0;
+        let high = data.length - 1;
+        let mid = 0;
+
+        while(low <= high){
+            mid = Math.floor((low + high) / 2)
+            if(data[mid] == target)
+                return data[mid]
+            else if(target < data[mid]){
+                high = mid - 1;
+            } else if(target > data[mid]){
+                low = mid + 1;
+            }
+        }
+        return -1 
+
     }
 }
 
 
 let tree = new BinarySearchArray([0,1,3,5,7,8,18,20,24]);
-console.log('Searching for 20 ', tree.search(20))
-console.log('Searching for 1 ', tree.search(1))
-console.log('Searching for 0 ', tree.search(0))
-console.log('Searching for 100 ', tree.search(100))
+console.log('Searching for 20 ', tree.search_recursion(20))
+console.log('Searching for 1 ', tree.search_recursion(1))
+console.log('Searching for 0 ', tree.search_recursion(0))
+console.log('Searching for 100 ', tree.search_recursion(100))
+console.log('--------------')
+console.log('Searching for 20 ', tree.search_iterative(20))
+console.log('Searching for 1 ', tree.search_iterative(1))
+console.log('Searching for 0 ', tree.search_iterative(0))
+console.log('Searching for 100 ', tree.search_iterative(100))
+
 
